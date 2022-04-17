21303d26c60b8c8e02fc623c3b9b593ce3ee4d37
Burns <brian.burns@unbounce.com>
2022-04-16 18:45:42 -0700

    Add manifest.js

---
+++ manifest.js
@@ -0,0 +1,24 @@
+export default {
+  appId: '@burns/popupsStickyBars',
+  version: '0.1',
+  name: 'Popups and Sticky Bars',
+  description: 'Embed popups and sticky bars.',
+  type: 'utility',
+  isBeta: true,
+  moreInfoUrl:
+    'https://documentation.unbounce.com/hc/en-us/articles/360020389312-Installing-Your-Popup-Sticky-Bar-on-an-Unbounce-Landing-Page',
+  iconUrl: 'https://app-assets-unbounce-com.s3.amazonaws.com/content-smart-builder-apps/popups-sticky-bars/icon.svg',
+  categories: ['script'],
+  files: [],
+  settings: {
+    embedCode: {
+      type: 'string',
+      label: 'Enter the embed code from the overview of your popup or sticky bar',
+      required: true,
+      placeholder: "<script src='https://12345678b4488af2f8c8bf2f54287.js.ubembed.com' async></script>",
+    },
+  },
+  externalScripts: [
+    'https://app-assets-unbounce-com.s3.amazonaws.com/content-smart-builder-apps/app-reviews/popups-sticky-bars/index.js',
+  ],
+};
